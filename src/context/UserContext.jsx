13c949a07e7dbb815a5e203adad0ecde2325689c
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2025-07-29 23:17:40 +0530

    Changes in the Context

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -9,6 +9,7 @@ export const UserContext = createContext({
 export const UserProvider = ({children})=>{
     const [userId , setUserId] = useState(null);
     const [userRole , setUserRole] = useState(null);
+    const [isLoggedIn , setIsLoggedIn] = useState(false);
 
     const fetchUser = async ()=>{
         try{
@@ -16,6 +17,7 @@ export const UserProvider = ({children})=>{
             if(response.status==200){
                 setUserId(response.data.email);
                 setUserRole(response.data.role);
+                setIsLoggedIn(true);
             }
         }catch(error){
 
@@ -26,7 +28,7 @@ export const UserProvider = ({children})=>{
         fetchUser();
     },[])
 
-    const value = {userId , userRole}
+    const value = {userId , userRole, isLoggedIn , setIsLoggedIn}
     console.log(value);
 
     return(
