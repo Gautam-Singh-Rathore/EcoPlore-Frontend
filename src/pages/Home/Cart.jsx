b8aa1a9498eb3e3f4622b0f0c5e1abd3c6b568c7
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2025-08-16 17:13:50 +0530

    Cart page changes

--- src/pages/Home/Cart.jsx
+++ src/pages/Home/Cart.jsx
@@ -1,120 +1,117 @@
-import React, { useEffect, useState } from 'react';
-import CartCard from '../../Components/CartCard';
-import Header from '../../Components/Home/Header';
-import MyLoader from '../../utils/MyLoader';
-import axiosInstance from '../../api/axiosInstance';
-import toast from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import CartCard from "../../Components/CartCard";
+import Header from "../../Components/Home/Header";
+import MyLoader from "../../utils/MyLoader";
+import axiosInstance from "../../api/axiosInstance";
+import toast from "react-hot-toast";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
- 
+  const [isLoading, setIsLoading] = useState(false);
 
   // Increase Quantity
-  const handleIncrease = async (id,itemQuantity) => {
+  const handleIncrease = async (id, itemQuantity) => {
     setIsLoading(true);
     try {
-        const response = await axiosInstance.post(`/private/cart/edit`,{
-          id : id,
-          quantity : itemQuantity+1
-        })
-        if(response.status == 200){
-          toast.success(`Item Quantity Updated Successfully`);
-          getCartItems();
-        }
+      const response = await axiosInstance.post(`/private/cart/edit`, {
+        id: id,
+        quantity: itemQuantity + 1,
+      });
+      if (response.status == 200) {
+        toast.success(`Item Quantity Updated Successfully`);
+        getCartItems();
+      }
     } catch (error) {
-      toast.error(error?.response?.data?.msg || "Failed to increase item quantity");
-       console.error("Error in Increasing Cart Item Quantity",error);
-    } 
-    finally{
+      toast.error(
+        error?.response?.data?.msg || "Failed to increase item quantity"
+      );
+      console.error("Error in Increasing Cart Item Quantity", error);
+    } finally {
       setIsLoading(false);
     }
   };
 
   // Decrease Quantity (minimum 1)
-  const handleDecrease = async (id,itemQuantity,cartId) => {
-   setIsLoading(true);
-   if(itemQuantity <=1){
-     return handleRemove(cartId);
-   }
-   try {
-        const response = await axiosInstance.post(`/private/cart/edit`,{
-          id : id,
-          quantity : itemQuantity-1 
-        })
-        if(response.status == 200){
-          toast.success(`Item Quantity Updated Successfully`);
-          getCartItems();
-        }
-   } catch (error) {
-      console.error("Error in Decreasing Cart Item Quantity",error);
-   }finally{
-    setIsLoading(false);
-   }
+  const handleDecrease = async (id, itemQuantity, cartId) => {
+    setIsLoading(true);
+    if (itemQuantity <= 1) {
+      return handleRemove(cartId);
+    }
+    try {
+      const response = await axiosInstance.post(`/private/cart/edit`, {
+        id: id,
+        quantity: itemQuantity - 1,
+      });
+      if (response.status == 200) {
+        toast.success(`Item Quantity Updated Successfully`);
+        getCartItems();
+      }
+    } catch (error) {
+      console.error("Error in Decreasing Cart Item Quantity", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   // Remove Item
   const handleRemove = async (cartid) => {
     setIsLoading(true);
     try {
-          const response = await axiosInstance.delete(`/private/cart/remove/${cartid}`);
-          if(response.status == 200){
-            toast.success("Item Removed Successfully");
-            getCartItems();
-          }
+      const response = await axiosInstance.delete(
+        `/private/cart/remove/${cartid}`
+      );
+      if (response.status == 200) {
+        toast.success("Item Removed Successfully");
+        getCartItems();
+      }
     } catch (error) {
-        console.error("Error in Removing Cart Item",error);
-    }
-    finally{
+      console.error("Error in Removing Cart Item", error);
+    } finally {
       setIsLoading(false);
     }
   };
 
-   const getCartItems = async ()=>{
-     setIsLoading(true);
-     try {
-           const response = await axiosInstance.get(`/private/cart/get`);
-           if(response.status == 200){
-             setCartItems(response.data);
-           }
-     } catch (error) {
-       console.error("Error in Fetching Cart Items ",error);
-     }
-     finally{
+  const getCartItems = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axiosInstance.get(`/private/cart/get`);
+      if (response.status == 200) {
+        setCartItems(response.data);
+      }
+    } catch (error) {
+      console.error("Error in Fetching Cart Items ", error);
+    } finally {
       setIsLoading(false);
-     }
-   }
+    }
+  };
 
-useEffect(()=>{
-   getCartItems();
-},[]);
+  useEffect(() => {
+    getCartItems();
+  }, []);
 
-   if(isLoading){
+  if (isLoading) {
+    return <MyLoader />;
+  }
+
+  if (cartItems.length == 0) {
     return (
-      <MyLoader/>
-    )
-   }
-
-   if(cartItems.length == 0){
-  
-        return (
-            <div className="w-full text-center py-10 text-gray-500">
-              <div className='mb-12'>
-                      <Header  heading="Cart"/>
-                      </div>
-             <div ></div>
-              No Product Found
-            </div>
-          );
-   }
+      <div className="w-full text-center py-10 text-gray-500">
+        <div className="mb-12">
+          <Header heading="Cart" />
+        </div>
+        <div></div>
+        No Product Found
+      </div>
+    );
+  }
 
   return (
-    <div >
-          <div className='mb-12'>
-          <Header  heading="Cart"/>
-          </div>
-      <div >
-      {cartItems.map((product) => (
+    <div>
+      <div className="mb-12">
+        <Header heading="Cart" />
+      </div>
+      <div>
+        {/* {cartItems.map((product) => (
         <CartCard
           key={product.productId}
           product={product}
@@ -123,7 +120,42 @@ useEffect(()=>{
           onDecrease={() => handleDecrease(product.productId,product.quantity,product.cartItemId)}
           onRemove={() => handleRemove(product.cartItemId)}
         />
-      ))}
+      ))} */}
+        {cartItems.map((product) => (
+          <CartCard
+            key={product.productId}
+            product={product}
+            quantity={product.quantity}
+            onIncrease={() =>
+              handleIncrease(product.productId, product.quantity)
+            }
+            onDecrease={() =>
+              handleDecrease(
+                product.productId,
+                product.quantity,
+                product.cartItemId
+              )
+            }
+            onRemove={() => handleRemove(product.cartItemId)}
+          />
+        ))}
+
+        {/* Total Price and Checkout Button */}
+        <div className="w-full flex flex-col items-center mt-8">
+          <div className="text-xl sm:text-2xl font-semibold mb-4">
+            Total: ₹
+            {cartItems.reduce(
+              (acc, item) => acc + item.price * item.quantity,
+              0
+            )}
+          </div>
+          <button
+            className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700"
+            onClick={() => alert("Proceeding to checkout...")} // replace with actual logic later
+          >
+            Checkout
+          </button>
+        </div>
       </div>
     </div>
   );
