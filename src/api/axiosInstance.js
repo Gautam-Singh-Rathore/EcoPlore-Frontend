106b15c71a0c503428cf565bafe6ebd695b5ee1f
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2025-08-24 12:26:53 +0530

    Chnages

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1', 
+  baseURL: 'http://api.greenplore.com/api/v1', 
   withCredentials: true
 });
 
