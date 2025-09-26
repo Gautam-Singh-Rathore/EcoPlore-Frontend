54fe70e56904074318c2f21648db0f4d8b4bc19d
Gautam Singh Rathore <gautamsingh25062005@gmail.com>
2025-09-26 22:07:07 +0530

    Updates

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://api.greenplore.com/api/v1', 
-  baseURL : 'http://localhost:8080/api/v1',
+  baseURL: 'http://localhost:8080/api/v1', 
   withCredentials: true
 });
 
