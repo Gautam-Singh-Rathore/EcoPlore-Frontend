8bbad366e900d0a2dfb14368d38cb23b807e298e
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-09-27 09:58:34 +0530

    user profile

--- src/pages/Home/Profile.jsx
+++ src/pages/Home/Profile.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import AvatarCard from '../../Components/Profile/AvatarCard'
 import OptionsCard from '../../Components/Profile/OptionsCard'
@@ -11,6 +11,7 @@ import { useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
 import toast from 'react-hot-toast'
 import axiosInstance from '../../api/axiosInstance'
+import ProfileEditForm from '../../Components/Buyer/ProfileEditForm'
 
 
 // Dummy components for right-side content
@@ -25,7 +26,13 @@ const Profile = () => {
   const navigate = useNavigate();
   const [selectedPage, setSelectedPage] = useState('orders');
   const { setIsLoggedIn, setUserId,setUserRole} = useContext(UserContext);
-    const [isLoading , setIsLoading] = useState(false);
+  const [isLoading , setIsLoading] = useState(false);
+  const [editMoode, setEditMode] = useState(false);
+  const [profile,setProfile] = useState({firstName: "",
+  lastName: "",
+  mobile: "",
+  email: "",
+  createdAt: "",});
     
 
 
@@ -38,6 +45,22 @@ const Profile = () => {
       default: return <div>Select an option</div>;
     }
   };
+
+const getProfile = async ()=>{
+  try {
+     const response = await axiosInstance.get('/private/profile');
+
+     if(response.status === 200){
+      setProfile(response.data);
+     }
+  } catch (error) {
+      const message =
+        error?.response?.data?.msg ||
+        error?.response?.data ||
+        ' failed to get profile data'
+      console.error(message);
+    }
+}
 
   const handleNavigation = (page) => {
     if (window.innerWidth < 1024) { // mobile device
@@ -69,7 +92,29 @@ const Profile = () => {
       setIsLoading(false);
     }
   }
+const handleEditProfile = async (updatedData)=>{
+  setIsLoading(true);
+     try {
+        const response = await axiosInstance.post('/private/profile/edit-customer',updatedData);
+        if(response.status === 200){
+          toast.success("Profile Updated Successfully");
+          getProfile();
+        }
+     } catch (error) {
+        const message =
+        error?.response?.data?.msg ||
+        error?.response?.data ||
+        'Failed to edit profile';
+      toast.error(message);
+     }finally{
+      setIsLoading(false);
+     }
+}
+  
 
+  useEffect(()=>{
+   getProfile();
+  },[])
 
   if(isLoading){
     return(
@@ -80,20 +125,33 @@ const Profile = () => {
   return (
     <div className='flex h-screen bg-slate-50 overflow-hidden'>
 
+      
+
       {/* Left Sidebar */}
       <div className='bg-white h-full w-full lg:w-1/3 pb-4 overflow-hidden'>
+     
         <AvatarCard
-          firstName="Gaurav"
-          lastName="Rawat"
-          mobile="+91 9876543210"
-          email="gaurav@example.com"
-          createdAt="2024-06-16"
+          
+          firstName={profile.firstName}
+          lastName={profile.lastName}
+          mobile={profile.mobile}
+          email={profile.email}
+          createdAt={
+    profile.createdAt
+      ? new Date(profile.createdAt).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "2-digit",
+        })
+      : ""
+  }
         />
 
         <div className="flex items-center justify-center p-4 px-2 gap-2">
           <BoxCard icon={Package} label="Orders" onClick={() => handleNavigation('orders')} />
           <BoxCard icon={Heart} label="Wishlist" onClick={() => handleNavigation('wishlist')} />
         </div>
+         <OptionsCard label="Edit Profile" onClick={()=>{setEditMode(true)}} />
         <OptionsCard label="Cart" onClick={() => handleNavigation('cart')} />
         <OptionsCard label="Logout" onClick={handleLogout} />
       </div>
@@ -102,6 +160,16 @@ const Profile = () => {
       <div className='hidden lg:block w-2/3 p-4 overflow-y-auto h-full'>
         {renderPage()}
       </div>
+      
+      { editMoode && (
+         <ProfileEditForm
+     initialData={profile}
+      onClose={()=>{setEditMode(false)}}
+      onSubmit={handleEditProfile}
+    />
+      )
+
+      }
 
     </div>
   );
